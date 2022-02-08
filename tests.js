b8f234fd9ac6ebc87c569a915917eee2ef3250f8
Tyreik Williams <tyreik.mm.williams@gmail.com>
2022-02-08 16:04:51 -0500

    fork repo and unit testing

--- tests.js
+++ tests.js
@@ -1,15 +1,28 @@
 // Unit tests for the helloWorld function
-describe('helloWorld', function() {
-    it('should be a defined function', function() {
-      expect(typeof helloWorld).toBe('function');
+//writre the code in js
+/*describe('helloWorld', function() { //  function is the call back function
+    it('should be a defined function', function() {  //what is descripton of test
+      expect(typeof helloWorld).toBe('function'); // expecting hellow world to be function
     });
     it('should return a string when called', function() {
-      expect(typeof helloWorld()).toBe("string");
+      expect(typeof helloWorld()).toBe("string"); //expecting helloworld to return a string ex. return "";
     });
     it('should return the string "Hello, World!" when executed', function() {
       expect(helloWorld()).toBe("Hello, World!");
     });
     it("should never return 'undefined' when called", function() {
-      expect(helloWorld()).not.toBe(undefined);
+      expect(helloWorld()).not.toBe(undefined); // only returns a string so nothing to do here
     });
 });
+*/
+
+// Unit tests for the sayHello function
+//writre the code in js
+
+//exercise 1
+describe('sayHello', function() {
+});
+ //   it('', function() {
+//expect(typeof helloWorld()).toBe("");
+//});
+
